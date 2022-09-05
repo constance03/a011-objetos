520e13a842f330fc36e791807704938b552a2740
constance03 <maria-constance@hotmail.com>
2022-09-05 15:53:54 -0300

    exercicios feitos

---
+++ exercicio1/script.js
@@ -0,0 +1,28 @@
+// Crie um array vazio chamado sacolao, e siga os passos abaixo:
+const sacolao = []
+
+// a) Crie três objetos que vão representar frutas do sacolão. Os objetos devem ter as seguintes propriedades:
+// nome: string;
+// preco: number;
+// disponibilidade: boolean;
+const fruta1 = {
+    nome: "pera", 
+    preco: 3, 
+    disponibilidade: true
+}
+const fruta2 = {
+    nome: "maca", 
+    preco: 4, 
+    disponibilidade: false
+}
+const fruta3 = {
+    nome: "uva", 
+    preco: 6, 
+    disponibilidade: true
+}
+
+// b) Adicione os objetos ao array sacolao utilizando o método push()
+sacolao.push(fruta1, fruta2, fruta3)
+
+// c) Imprima a o array sacolao, e certifique-se de que agora ela seja um array com três objetos
+console.log(sacolao);
